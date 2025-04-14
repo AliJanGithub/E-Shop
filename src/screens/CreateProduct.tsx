175e5aa34b52c9f3e6853e3107b59c5763a02d61
ALI JAN <133298273+AliJanGithub@users.noreply.github.com>
2025-04-14 20:53:33 +0500

    updated

--- src/screens/CreateProduct.tsx
+++ src/screens/CreateProduct.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react'
+import  { ChangeEvent, FormEvent, useState } from 'react'
 import { Url } from '../../contant'
 import { useNavigate } from 'react-router-dom'
 
